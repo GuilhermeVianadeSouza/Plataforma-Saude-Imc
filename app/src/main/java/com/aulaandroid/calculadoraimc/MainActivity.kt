f6d9cc2eb20e2f43787bcbeb87f6a394faa0133f
GUILHERME VIANA DE SOUZA <25132756@dsn127m1246728.cfp127aluno.local>
2026-03-11 10:34:10 -0300

    Ajustando row para card e aplicando cores do app ao botoes

--- app/src/main/java/com/aulaandroid/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/aulaandroid/calculadoraimc/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,6 +26,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedButton
@@ -48,6 +50,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -187,9 +190,13 @@ fun IMCScreen(modifier: Modifier = Modifier) {
                             imc = pesoConvertido / (alturaConvertida * alturaConvertida)
 
                         },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(R.color.cor_app)
+                        )
                     ) {
                         Text(text = "Calcular",
-                            fontSize = 24.sp)
+                            fontSize = 24.sp,
+                            color = Color.White)
                     }
                     OutlinedButton(
                         onClick = {
@@ -197,25 +204,46 @@ fun IMCScreen(modifier: Modifier = Modifier) {
                             peso = ""
                             imc = 0.0
                         },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(R.color.cor_app)
+                        )
                     ) {
                         Text(text = "Limpar campos",
-                            fontSize = 24.sp)
+                            fontSize = 24.sp,
+                            color = Color.White)
                     }
                 }
             }
         }
-        Row(
-            modifier = modifier.width(250.dp)
-                .height(250.dp)
-                .clip(RoundedCornerShape(16.dp))
-                .background(corFundo)
-                .padding(horizontal = 34.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+        Card(
+            modifier = modifier
+                .width(350.dp)
+                .height(200.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = corFundo
+            ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
         ) {
-            Text(text = "${imcFormatado}, ${classificacao}",
-                color = Color.White,
-                fontSize = 16.sp)
+            Row(
+                modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = imcFormatado,
+                    color = Color.White,
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = classificacao,
+                    color = Color.White,
+                    fontSize = 28.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 }
